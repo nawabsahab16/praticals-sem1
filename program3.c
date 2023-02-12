64f3b087a438f053ed5e2f5756e623d1f3fbac29
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-02-12 14:38:03 +0530

    👉🏾 Program is executed successfully

---
+++ program3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+ int main()
+ {
+    int a,b, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a,&b);
+
+    sum= a + b;
+    printf("Sum of two numbers : %d", &sum);
+
+
+   return 0;
+ }
