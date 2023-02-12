b1f48b932f51be6e954f5cefdf2b8074b8d8033e
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-02-12 14:47:34 +0530

    👉🏾 Program is executed successfully

---
+++ program5.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main() {
+    float p,r,t,si,ci;
+    clrscr();
+
+    printf("Enter principal amount (p): ");
+    scanf("%f", &p);
+    printf("Enter time in a year(t):");
+    scanf("%f", &t);
+    printf("Enter rate in percent(r):");
+    scanf("%f", &r);
+
+    /* Calculating simple interest */
+
+    si=(p*t*r)/100.0;
+
+    /*  Calculating compund interest */
+
+    ci= p* (pow(1+r/100, t)-1);
+
+
+    printf("Simple interest = %0.3f\n", si);
+
+    printf("Compund interest= %0.3f", ci);
+
+
+    getch();
+     return 0;
+
+
+}
